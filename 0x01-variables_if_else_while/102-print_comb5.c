49d3c2cfd2849fa069c97c589289a87c4596cd00
Akuya Ekorot <akuyaekorot@gmail.com>
2022-12-10 03:14:37 +0300

    software is eatng the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+* main - entry point. Prints all possible combinations
+* of two two-digit numbers.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int a = 0, b = 0;
+	int c = 0, d = 0;
+
+	while (a < 10)
+	{
+		while (b < 10)
+		{
+			while (c < 10)
+			{
+				while (d < 10)
+				{
+					if (c >= a && d > b)
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(' ');
+						putchar(c + '0');
+						putchar(d + '0');
+
+						!(a == 9 && b == 8) && putchar(',');
+						!(a == 9 && b == 8) && putchar(' ');
+					}
+					d++;
+				}
+				d = 0;
+				c++;
+			}
+			c = 0;
+			b++;
+		}
+		b = 0;
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
