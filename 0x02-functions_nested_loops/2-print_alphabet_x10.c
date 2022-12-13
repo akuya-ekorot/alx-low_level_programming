baf27eb5522ca68abcfa090814f05c321fdcf27b
Akuya Ekorot <akuyaekorot@gmail.com>
2022-12-13 18:12:46 +0300

    alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+* print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
+
+/**
 * print_alphabet_x10 - Prints the alphabet 10 times in lowercase,
 * followed by a new line.
 *
