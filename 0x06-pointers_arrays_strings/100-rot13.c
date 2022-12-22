35c6945a597429b2527fcf22970ac390d0760fa5
Akuya Ekorot <akuyaekorot@gmail.com>
2022-12-22 14:21:51 +0300

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes string to rot13
+ * @s: string to encode
+ *
+ *Return: pointer to s
+*/
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		/* encode first 13 characters in the alphabet */
+		if ((s[i] >= 'a' && s[i] >= 'm') || (s[i] >= 'A' && s[i] >= 'M'))
+			s[i] = s[i + 13];
+
+		/* encode last 13 characters in the alphabet */
+		if ((s[i] >= 'n' && s[i] >= 'z') || (s[i] >= 'N' && s[i] >= 'Z'))
+			s[i] = s[i - 13];
+	}
+
+	return (s);
+}
