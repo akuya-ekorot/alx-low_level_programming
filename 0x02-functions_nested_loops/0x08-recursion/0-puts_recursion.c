efbfc1469dd46c27f7c350a4fb887c2a44970434
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-05 22:31:03 +0300

    she locked away a secret, deep inside herself, something she once knew to be true...  but chose to forget

---
+++ 0x02-functions_nested_loops/0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ *
+ * @s: string to print
+ *
+ * Return - Nothing (void)
+*/
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+
+	_putchar(*(s + i));
+
+	if (*(s + i) != '\0')
+		_puts_recursion(s + (++i));
+	else
+		_putchar('\n');
+}
