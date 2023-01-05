d075d573fd461ee04d1e81e035ba8dff84d3b811
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-05 22:37:36 +0300

    she locked away a secret, deep inside herself, something she once knew to be true...  but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,9 +14,10 @@ void _puts_recursion(char *s)
 	i = 0;
 
 	_putchar(*(s + i));
+	i++;
 
 	if (*(s + i) != '\0')
-		_puts_recursion(s + (++i));
+		_puts_recursion(s + i);
 	else
 		_putchar('\n');
 }
