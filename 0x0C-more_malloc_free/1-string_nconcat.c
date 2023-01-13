6f13384e9f35a000340026f28f417307fe01a3bc
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-14 00:47:35 +0300

    rewrite string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,84 +1,54 @@
 #include "main.h"
 
 /**
-* _strcpy - copies a string pointed to by src, including the terminating null
-* byte, to the buffer pointed to by dest
-*
-* @src: string
-* @dest: buffer
-*
-* Return: pointer to dest
-*/
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; *(src + i) != '\0'; i++)
-		*(dest + i) = *(src + i);
-
-	return (dest);
-}
-
-/**
-* _strlen - finds length of a string
-*
-* @s: string to check
-*
-* Return: unsigned int, length of string
-*/
-unsigned int _strlen(char *s)
-{
-	unsigned int i, j;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-		j++;
-
-	return (j);
-}
-
-/**
-* string_nconcat - concatenates two strings
-*
-* @s1: first string
-* @s2: second string
-* @n: n bytes of s2 to include in the concatenated string
-*
-* Return: newly allocated space in memory containing s1 and first n bytes
-* of s2, or NULL if malloc fails
+ * string_nconcat - concatenates two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: n bytes of s2 to include
+ *
+ * Return: pointer to newly allocated space in memory
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, size, j;
+	unsigned int i, size_s1, size_s2;
 	char *str;
 
-	if (s1 != NULL && *s1 != '\0')
+	size_s1 = 0;
+	size_s2 = 0;
+
+	/* find size to allocate to str */
+	if (s1 != NULL)
 	{
-		i = _strlen(s1);
-		size = n + i + 1;
-		str = malloc(sizeof(char) * size);
-
-		if (str == NULL)
-			return (NULL);
-
-		str = _strcpy(str, s1);
-	}
-	else
-	{
-		i = 0;
-		size = n + 1;
-		str = malloc(sizeof(char) * (size + 1));
-
-		if (str == NULL)
-			return (NULL);
+		for (i = 0; *(s1 + i); i++)
+			size_s1++;
 	}
 
 	if (s2 != NULL)
 	{
-		for (j = 0; i < size - 1 && *(s2 + j) != '\0'; j++, i++)
-			*(str + i) = *(s2 + j);
+		for (i = 0; *(s2 + i); i++)
+			size_s2++;
+
+		if (size_s2 > n)
+			size_s2 = n;
 	}
 
-	*(str + i) = '\0';
+	/* allocate memory to str */
+	str = malloc((size_s1 + size_s2 + 1) * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+
+	/* copy s1 to str */
+	for (i = 0; i < size_s1; i++)
+		*(str + i) = *(s1 + i);
+
+	/* copy s2 to str */
+	for (i = 0; i < size_s2; i++, size_s1++)
+		*(str + size_s1) = *(s2 + i);
+
+	/* add terminating null byte */
+	*(str + size_s1) = '\0';
 
 	return (str);
 }
