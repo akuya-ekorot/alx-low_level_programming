3a1212b0b56de24cb1733fea9cf4cfbefd9177ec
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-17 15:40:04 +0300

    allocate size to copies of strings

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 /**
+* _strlen - returns the length of a string
+* @s: pointer to string
+* Return: length of string
+*/
+int _strlen(char *s)
+{
+	int i, len;
+
+	len = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		len++;
+
+	return (len);
+}
+
+/**
 * _strcpy - copies a string pointed to by src, including the terminating null
 * byte, to the buffer pointed to by dest
 *
@@ -33,6 +50,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *n, *o;
 	dog_t *new;
+
+	n = malloc(sizeof(char) * _strlen(name));
+	o = malloc(sizeof(char) * _strlen(owner));
 
 	n = _strcpy(n, name);
 	o = _strcpy(o, owner);
