1da47077879b7dd0affcb87fe34d6dc2477b5db2
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-17 15:35:16 +0300

    make copies of parameters name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 
 /**
+* _strcpy - copies a string pointed to by src, including the terminating null
+* byte, to the buffer pointed to by dest
+*
+* @src: string
+* @dest: buffer
+*
+* Return: pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+		*(dest + i) = *(src + i);
+	*(dest + i) = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: string, name of the dog
  * @age: float, the age of the dog
@@ -14,8 +34,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *n, *o;
 	dog_t *new;
 
-	n = name;
-	o = owner;
+	n = _strcpy(n, name);
+	o = _strcpy(o, owner);
 
 	new = malloc(sizeof(dog_t));
 
