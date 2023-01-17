49cfb9bd7368210287c9d93c4d4e4555c6192eb3
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-17 13:58:12 +0300

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,6 +16,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
 
+	if (d == NULL)
+		exit(EXIT_FAILURE);
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
