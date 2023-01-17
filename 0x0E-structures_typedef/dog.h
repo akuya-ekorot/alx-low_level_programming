00c1441c35ad143f68e8e0f9e126a7c0578fb111
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-17 13:45:09 +0300

    add descriptions to struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - describes a dog
+ * @name: Name of the dog, string
+ * @age: Age of the dog, float
+ * @owner: Owener of the dog, string
+ */
 struct dog
 {
 		char *name;
