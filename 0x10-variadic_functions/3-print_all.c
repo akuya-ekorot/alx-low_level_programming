d1c7772a0092e5967c62e8efbb0d6ab2f0fdd56b
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-21 16:38:45 +0300

    0x10: Print All

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 
 /**
  * print_all - prints anything
@@ -9,12 +8,85 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i, j;
+	identifier ids[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
+		{NULL, NULL}
+	};
 	va_list ap;
-	char *types = "cifs";
+	int i, j;
+	char *separator = "";
 
 	va_start(ap, format);
 
+	i = 0;
 
+	while (format && format[i])
+	{
+		j = 0;
+
+		while (ids[j].format)
+		{
+			if (*ids[j].format == format[i])
+			{
+				printf("%s", separator);
+				ids[j].print(ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+
+	printf("\n");
 	va_end(ap);
+}
+
+/**
+ * print_c - prints a character
+ * @list: va_list
+ *
+ */
+void print_c(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * print_i - prints an integer
+ * @list: va_list
+ *
+ */
+void print_i(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_f - prints an integer
+ * @list: va_list
+ *
+ */
+void print_f(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * print_s - prints a string
+ * @list: va_list
+ *
+ */
+void print_s(va_list list)
+{
+	char *str;
+
+	str = va_arg(list, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+
+	printf("%s", str);
 }
