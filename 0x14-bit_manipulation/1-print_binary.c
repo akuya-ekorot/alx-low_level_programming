38ef0796dd0471cd7b5ba1b49b727e284e0393a7
Akuya Ekorot <akuyaekorot@gmail.com>
2023-02-06 16:03:16 +0300

    change get bit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,15 +10,16 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int mask;
 
-	mask = 0x8000000000000000;
+	mask = 1;
+	mask <<= 63;
 
-	while (mask != 0 && (n & mask) == 0)
+	while (mask && (n & mask) == 0)
 		mask >>= 1;
 
 	if (mask == 0)
 		_putchar('0');
 
-	while (mask != 0)
+	while (mask)
 	{
 		_putchar((n & mask) == 0 ? '0' : '1');
 		mask >>= 1;
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask;
 
-	mask = 1 << index;
+	mask = 1ul << index;
 
 	return ((mask & n) == 0 ? 0 : 1);
 }
