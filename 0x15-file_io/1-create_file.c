ee3f4e8d4561bbe5544bf4a452e242899cbfb8a6
Akuya Ekorot <akuyaekorot@gmail.com>
2023-02-08 18:44:15 +0300

    remove null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 			;
 	}
 
-	written = write(fd, text_content, bytes);
+	written = write(fd, text_content, bytes - 1);
 
 	if (written == -1)
 	{
