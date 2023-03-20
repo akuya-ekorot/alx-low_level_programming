5a249dc68d09477e0fe1a867f3f98b819ec38bfa
Akuya Ekorot <akuyaekorot@gmail.com>
2023-03-20 21:20:38 +0300

    fix betty issues for strspn.c

--- 0x18-dynamic_libraries/strspn.c
+++ 0x18-dynamic_libraries/strspn.c
@@ -1,6 +1,14 @@
+/**
+ * _strspn - placeholder for the strspn function
+ * @s: string
+ * @accept: string
+ *
+ * Return: An unsigned int
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int x;
+
 	x = 1;
 	return (x);
 }
