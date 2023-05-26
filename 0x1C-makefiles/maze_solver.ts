fbf2c445c19d454f12bb800b6101e4828d54ed90
Akuya Ekorot <akuyaekorot@gmail.com>
2023-05-26 13:11:53 +0300

    found the perimeter

---
+++ 0x1C-makefiles/maze_solver.ts
@@ -0,0 +1,11 @@
+type Wall = "#";
+
+function walk(maze: string[], wall: Wall, curr: Point, end: Point): boolean {
+  //1. Base case
+  // off the map
+  
+}
+
+export default function solve(maze: string[], wall: Wall, start: Point, end: Point): Point[] {
+  
+}
