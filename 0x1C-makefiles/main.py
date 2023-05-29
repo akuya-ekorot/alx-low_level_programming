62b852f9d07ac21db63d6ed61b6ad6dfdf902956
Akuya Ekorot <akuyaekorot@gmail.com>
2023-05-29 12:26:25 +0300

    test cases

--- 0x1C-makefiles/main.py
+++ 0x1C-makefiles/main.py
@@ -5,13 +5,54 @@
 island_perimeter = __import__("5-island_perimeter").island_perimeter
 
 if __name__ == "__main__":
+    # alx case 1
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+    ]
+    for row in grid:
+        print(row)
+    print(island_perimeter(grid))
+
+    # alx case 2
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+    ]
+    for row in grid:
+        print(row)
+    print(island_perimeter(grid))
+
+    # alx case 3
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+    ]
+    for row in grid:
+        print(row)
+    print(island_perimeter(grid))
+
+    # alx case 4
+    grid = [
+        [0],
+        [0],
+        [0],
+    ]
+    for row in grid:
+        print(row)
+    print(island_perimeter(grid))
 
     # grid is fully land
     grid = [
         [1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1]
     ]
 
     for row in grid:
@@ -19,12 +60,27 @@ if __name__ == "__main__":
 
     print(island_perimeter(grid))
 
-    # grid is fully land
+    # grid has land till the edge of the border
     grid = [
         [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
         [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 1, 1, 1],
+    ]
+
+    for row in grid:
+        print(row)
+
+    print(island_perimeter(grid))
+
+    # grid is surrounded by water
+    grid = [
+        [0, 1, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1, 1],
         [0, 1, 1, 1, 0, 0],
-        [0, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0],
     ]
 
     for row in grid:
