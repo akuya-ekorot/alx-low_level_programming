971eaeeb612c44d38512cc9eb37ac39d5597635b
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-10 10:15:22 +0300

    linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* Libraries */
+#include <stdio.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS */
